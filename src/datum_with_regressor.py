cc5d054cb6ad398d7af1e2f400d938c6ea955caf
Nawel Medjkoune <nawel@users.noreply.github.com>
2017-01-03 22:49:38 +0100

    soustraire le mean

--- src/datum_with_regressor.py
+++ src/datum_with_regressor.py
@@ -18,10 +18,17 @@ print "Loading the data to predict ..."
 dataTest=pickle.load(open("/Users/ozad/Desktop/axa_data_challenge-master/test_DataFrame.obj","rb"))
 
 labels = data['CSPL_RECEIVED_CALLS'].unique()
+
+#soustraire la moyenne
+mean_calls=data.groupby(['ASS_ASSIGNMENT', 'DAY_WE_DS','hour', 'minute']).mean().reset_index()
+data['MEAN_calls'] = pd.merge(data, mean_calls, how='left', on=['ASS_ASSIGNMENT', 'DAY_WE_DS','hour', 'minute'])['CSPL_RECEIVED_CALLS_y']
+data['CSPL_RECEIVED_CALLS']=data['CSPL_RECEIVED_CALLS']-data['MEAN_calls']
+dataTest['MEAN_calls'] = pd.merge(dataTest, mean_calls, how='left', on=['ASS_ASSIGNMENT', 'DAY_WE_DS','hour', 'minute'])['MEAN_calls']
+
 #### Load the X train and Y train
-Y_train=data['CSPL_RECEIVED_CALLS'][:300000]
-X_train=data[  [  'DAY_WE_DS',    'cod_ASS_ASSIGNMENT','year','month','day','hour','minute']][:300000]
-X_test=dataTest[[  'DAY_WE_DS',    'cod_ASS_ASSIGNMENT','year','month','day','hour','minute']]
+Y_train=data['CSPL_RECEIVED_CALLS']
+X_train=data[  [  'DAY_WE_DS','cod_ASS_ASSIGNMENT','year','month','day','hour','minute']]
+X_test=dataTest[[  'DAY_WE_DS','cod_ASS_ASSIGNMENT','year','month','day','hour','minute']]
 
 Y_train=np.array(Y_train)
 X_train=np.array(X_train)
@@ -36,9 +43,10 @@ reg.fit(X_train, Y_train)
 #### Prediction
 print "Prediction ..."
 Y_pred = reg.predict(X_test)
-
+#rajouter le mean
+Y_pred=Y_pred+dataTest['MEAN_calls']
 #### write the submission
 print "Write the submission ..."
-make_submission(dataTest,Y_pred)
+make_submission1(dataTest,Y_pred)
 
 print "End."
